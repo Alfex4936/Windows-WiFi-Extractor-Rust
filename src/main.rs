5968023242aacd3f3d946d3bfe93ab827da2aa66
seok <ikr@kakao.com>
2022-08-24 19:13:31 +0900

    Rust multithread version

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 use itertools::Itertools;
-use regex::RegexBuilder;
-use std::sync::Arc;
+use regex::{Regex, RegexBuilder};
 use std::thread;
-use std::time::Duration;
 use std::{collections::HashMap, error::Error};
 use subprocess::{Popen, PopenConfig, Redirection};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let re_wifi_profile = RegexBuilder::new(r"All User Profile\s+:\s(?P<profile>.*)$")
+use lazy_static::lazy_static;
+lazy_static! {
+    static ref RE_WIFI_PROFILE: Regex = RegexBuilder::new(r"All User Profile\s+:\s(?P<profile>.*)$")
         .multi_line(true)
         .build()
         .unwrap();
-    let re_wlan_key = RegexBuilder::new(r"Key Content\s+:\s(?P<ssid>.*)$")
+    static ref RE_WLAN_KEY: Regex = RegexBuilder::new(r"Key Content\s+:\s(?P<ssid>.*)$")
         .multi_line(true)
         .build()
         .unwrap();
+    // static ref found_ssids: Arc<Mutex<HashMap<String, String>>> =
+    //     Arc::new(Mutex::new(HashMap::new()));
+}
 
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Loading all WiFis saved on computer...");
     let cmd: Vec<&str> = r#"netsh wlan show profiles"#.split(" ").map(|s| s).collect();
 
@@ -42,9 +45,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let (out, _) = proc.communicate(None).unwrap();
     let profiles_stdout = out.unwrap();
 
-    let mut found_ssids = HashMap::new();
+    // let mut found_ssids = HashMap::new();
+    let mut found_ssids: HashMap<String, String> = HashMap::new();
 
-    for profile in re_wifi_profile.captures_iter(&profiles_stdout) {
+    for profile in RE_WIFI_PROFILE.captures_iter(&profiles_stdout) {
         let ssid = profile["profile"].trim().to_string();
         found_ssids.insert(ssid, String::new());
     }
@@ -56,42 +60,41 @@ fn main() -> Result<(), Box<dyn Error>> {
         std::process::exit(1);
     }
 
-    println!("Loading passwords...");
+    println!("Loading passwords...\n");
 
-    for (ssid, value) in found_ssids.iter_mut() {
-        // TODO - 멀티 쓰레딩으로 Go언어 GUI 버전처럼 바꾸기
+    thread::scope(|s| {
+        for (ssid, value) in found_ssids.iter_mut() {
+            s.spawn(move || {
+                let cmd = format!("netsh wlan show profile {ssid} key=clear");
+                let cmd: Vec<&str> = cmd.as_str().split(" ").map(|s| s).collect();
 
-        let cmd = format!("netsh wlan show profile {ssid} key=clear");
-        let cmd: Vec<&str> = cmd.as_str().split(" ").map(|s| s).collect();
+                let proc = Popen::create(
+                    &cmd,
+                    PopenConfig {
+                        stdout: Redirection::Pipe,
+                        stdin: Redirection::None,
+                        stderr: Redirection::None,
+                        ..Default::default()
+                    },
+                );
 
-        let proc = Popen::create(
-            &cmd,
-            PopenConfig {
-                stdout: Redirection::Pipe,
-                stdin: Redirection::None,
-                stderr: Redirection::None,
-                ..Default::default()
-            },
-        );
+                let mut proc = proc.expect("Failed to create process.");
 
-        let mut proc = match proc {
-            Ok(proc) => proc,
-            Err(e) => {
-                panic!("Failed to create process: {:?}", e)
-            }
-        };
+                let (out, _) = proc.communicate(None).unwrap();
+                let ssid_stdout = out.unwrap();
 
-        let (out, _) = proc.communicate(None).unwrap();
-        let ssid_stdout = out.unwrap();
-
-        for wifi in re_wlan_key.captures_iter(&ssid_stdout) {
-            let password = wifi["ssid"].trim().to_string();
-            *value = password;
+                for wifi in RE_WLAN_KEY.captures_iter(&ssid_stdout) {
+                    let password = wifi["ssid"].trim().to_string();
+                    *value = password;
+                }
+            });
         }
-    }
+    });
 
+    let mut i = 1;
     for (ssid, password) in found_ssids.iter().sorted() {
-        println!("Wifi: {}, Password: {}", ssid, password);
+        println!("\t{i}. {ssid}: \"{password}\"");
+        i += 1;
     }
-    Ok(())
+    std::process::exit(0);
 }
